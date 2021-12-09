c595f2dffc495ff4f396efba83ee173fc21392e1
Артём <56469391+ArtShp@users.noreply.github.com>
2021-12-09 17:23:17 +0300

    Basic app structure

--- Biology_NPK/app_test.py
+++ Biology_NPK/app_test.py
@@ -4,15 +4,21 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle('Title')
+        self.setGeometry(0, 0, 1280, 720)
+
+
 def application():
     app = QApplication(sys.argv)
-    window = QMainWindow()
-
-    window.setWindowTitle('8')
-    window.setGeometry(0, 0, 1280, 720)
+    window = Window()
 
     window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     application()
